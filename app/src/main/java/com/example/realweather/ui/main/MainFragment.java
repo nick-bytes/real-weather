4ad935d592349af41ee4605a58934f2654adb30b
Nick <emercnk@gmail.com>
2020-01-02 13:55:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/realweather/ui/main/MainFragment.java
@@ -0,0 +1,39 @@
+package com.example.realweather.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.realweather.R;
+
+public class MainFragment extends Fragment {
+
+	public static MainFragment newInstance() {
+		return new MainFragment();
+	}
+
+	private MainViewModel mViewModel;
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+		// TODO: Use the ViewModel
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+			@Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.main_fragment, container, false);
+	}
+
+}
